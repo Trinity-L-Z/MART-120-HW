2af5a85ed5fdca55e63719dd34125ec84c398d67
Trinity-L-Z <trinityzachariasen@gmail.com>
2025-03-27 09:58:39 -0500

    Update sketch.js

--- Week9/sketch.js
+++ Week9/sketch.js
@@ -4,7 +4,7 @@ function setup() {
 
 function draw() {
     background(220);
-    text('"Burnt Out", by Trinity Zachariasen', 300, 570);
+    text('"Wonderful!", by Trinity Zachariasen', 300, 570);
     circle(300, 100, 50);
     circle(400, 100, 50);
     triangle(360, 200, 360, 150, 275, 200);
