d87d2ab9400ae6cc6523a12722f75cf925a95658
Trinity-L-Z <trinityzachariasen@gmail.com>
2025-04-07 14:02:37 -0500

    week 10 hw

---
+++ Week10/sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+    createCanvas(600, 600);
+}
+
+function draw() {
+    background(220);
+    text('"The book I just finished reading", by Trinity Zachariasen', 300, 570);
+    circle(300, 100, 50);
+    circle(400, 100, 50);
+    triangle(360, 200, 360, 150, 275, 200);
+    line(260, 275, 400, 275);
+    line(400, 275, 425, 260);
+    line(260, 70, 270, 60);
+    line(270, 60, 330, 60);
+    line(360, 60, 420, 60);
+    line(420, 60, 440, 70);
+    ellipse(340, 330, 70, 50);
+    triangle(440, 175, 440, 150, 480, 150);
+    triangle(260, 175, 260, 150, 240, 150);
+    point(300, 100);
+    point(400, 100);
+    rect(75, 100, 150, 200);
+    text('Small Worlds', 120, 140);
+    text('by Caleb Azumah Nelson',85, 200);
+}
