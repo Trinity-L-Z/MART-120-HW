29b69d415836a1898dc791220b0b29914877ead3
Trinity-L-Z <trinityzachariasen@gmail.com>
2025-04-07 22:38:58 -0500

    Update sketch.js

--- Week10/sketch.js
+++ Week10/sketch.js
@@ -24,7 +24,7 @@ function setup() {
 
 function draw() {
     background(220);
-    textSize(10);
+    textSize(15);
     text('"The book I just finished reading", by Trinity Zachariasen', 300, 570);
     circle(x1, y, diameter);
     //this is the left eyeball
