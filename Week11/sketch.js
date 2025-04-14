55455fdec639f7213ef3865131c969fd4011b92d
Trinity-L-Z <trinityzachariasen@gmail.com>
2025-04-14 13:40:45 -0500

    Update sketch.js

--- Week11/sketch.js
+++ Week11/sketch.js
@@ -58,7 +58,7 @@ function draw() {
 
     // the moving shape
     fill(0, 100, 400);
-    circle(shapeX, shapeY, 10);
+    circle(shapeX, shapeY, 40);
     
     shapeXSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
     shapeYSpeed = Math.floor(Math.random() * (Math.floor(Math.random() * 5)) + 1);
