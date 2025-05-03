3905aa8d112f000e60831460b8d4c3b39efac691
Trinity-L-Z <trinityzachariasen@gmail.com>
2025-05-02 23:02:42 -0500

    Update sketch.js

--- finalproject/sketch.js
+++ finalproject/sketch.js
@@ -1,8 +1,17 @@
+let size = 50; let num = 3
+
 function setup() {
     createCanvas(400, 400, WEBGL);
 }
 
 function draw() {
     background(220);
-    box(50);
+    orbitControl();
+    
+    for (let i=0; i<num; i++) {
+        push();
+        translate(i*size, 0);
+        box(50);
+        pop();
+    }
 }
