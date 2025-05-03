e403fe664746434ae3fff98e6a8654519f828192
Trinity-L-Z <trinityzachariasen@gmail.com>
2025-05-02 20:14:24 -0500

    kjhgf

--- finalproject/sketch.js
+++ finalproject/sketch.js
@@ -2,6 +2,14 @@ let song;
 let fft; smoothing = 0.8; let bins = 512;
 let waveform = [];
 
+const audio = new Audio("machine.mp3");
+audio.addEventListener('canplaythrough', function() {
+    audio.play();
+});
+audio.addEventListener('error', function() {
+    console.error('Error loading audio:', audio.error);
+});
+
 function preload() {
     song = loadSound("machine.mp3")
 }
