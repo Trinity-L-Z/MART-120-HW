19e335a02f36d1036db7eafc9e452cf7a1af5163
Trinity-L-Z <trinityzachariasen@gmail.com>
2025-05-02 19:36:15 -0500

    final project work

--- finalproject/sketch.js
+++ finalproject/sketch.js
@@ -1,4 +1,6 @@
 let song;
+let fft; smoothing = 0.8; let bins = 512;
+let waveform = [];
 
 function preload() {
     song = loadSound("machine.wav")
@@ -7,8 +9,11 @@ function preload() {
 function setup() {
     createCanvas(400, 400);
     song.play();
+    fft = new p5.FFT(smoothing, bins);
 }
 
 function draw() {
     background(220);
+    waveform = fft.waveform();
+    print(waveform);
 }
