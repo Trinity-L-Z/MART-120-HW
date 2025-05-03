4930767af0143963f96105f1502609821b40a684
Trinity-L-Z <trinityzachariasen@gmail.com>
2025-05-03 09:35:58 -0500

    Update sketch.js

--- finalproject/sketch.js
+++ finalproject/sketch.js
@@ -1,14 +1,33 @@
 let size = 15; let num = 10
 let grid = [];
 
+let song; let fft;
+let spectrum = [];
+let distFromCenter = []:
+
+function preload() {
+    song = loadSound("machine.mp3");
+}
+
 function setup() {
     createCanvas(400, 400, WEBGL);
+    fft = new p5.FFT();
+
     for (let i=0; i<num; i++) {
         grid[i] = [];
         for (let j=0; j<num; j++) {
             grid[i][j] = [];
             for (let k=0; k<num; k++) {
                 grid[i][j][k] = floor(random(2));
+                
+                let offset = size/2 - num/2*size;
+                let x = i*size + offset;
+                let y = j*size + offset;
+                let z = k*size + offset;
+                let distance = dist(x, y, z, 0, 0, 0);
+
+                distFromCenter.push({x, y, z, distance});
+                print(distFromCenter);
             }
         }
     }
@@ -17,8 +36,10 @@ function setup() {
 function draw() {
     background(220);
     orbitControl();
+
+    spectrum = fft.analyze();
     
-    //for position in the center
+    //for positioning in the center
     let offset = size/2 -num/2 * size
     translate(offset, offset, offset);
     noFill();
