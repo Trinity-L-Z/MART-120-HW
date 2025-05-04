b038efaca14714afbd38bd0af405c44b42b9666f
Trinity-L-Z <trinityzachariasen@gmail.com>
2025-05-04 14:13:03 -0500

    Update script.js

--- finalproject/script.js
+++ finalproject/script.js
@@ -8,32 +8,47 @@ function preload() {
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    angleMode(DEGREES)
+
     button = createButton("play");
     button.mousePressed(togglePlaying);
+
     fft = new p5.FFT()
 }
 
 function draw() {
     background (0)
     stroke(255) //wave color
+    noFill()
+
+    translate(width / 2, height / 2)
 
     var wave = fft.waveform()
 
-    for (var i = 0; i < width; i++) {
-        var index = floor(map(i, 0, width, 0, wave.length))
+    for (var t = -1; t <= 1; t += 2) {
+        beginShape()
+        for (var i = 0; i < 180; i += 0.5) {
+            var index = floor(map(i, 0, 180, 0, wave.length))
 
-        var x = i
-        var y = wave[index] * 300 + height / 2
-        point(x, y)
-    }
+            var r = map(wave[index], -1, 1, 150, 350)
+
+            var x = r * sin(i) * t
+            var y = r * cos(i)
+            vertex(x, y)
+        }
+        endShape()
+        }
+
 }
 
 function togglePlaying() {
     if (!song.isPlaying()) {
         song.play();
         button.html("pause");
+        loop();
     } else {
         song.pause();
         button.html("play");
+        noLoop();
     }
 }
