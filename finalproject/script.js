a68fc0753117005dc640d85769695277f6ddf581
Trinity-L-Z <trinityzachariasen@gmail.com>
2025-05-04 13:52:00 -0500

    Update script.js

--- finalproject/script.js
+++ finalproject/script.js
@@ -1,4 +1,5 @@
 var song
+var fft
 
 
 function preload() {
@@ -9,10 +10,22 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     button = createButton("play");
     button.mousePressed(togglePlaying);
+    fft = new p5.FFT()
 }
 
 function draw() {
     background (0)
+    stroke(255) //wave color
+
+    var wave = fft.waveform()
+
+    for (var i = 0; i < width; i++) {
+        var index = floor(map(i, 0, width, 0, wave.length))
+
+        var x = i
+        var y = wave(index) * 300 + height / 2
+        point(x, y)
+    }
 }
 
 function togglePlaying() {
