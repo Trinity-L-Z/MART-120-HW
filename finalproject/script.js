f6f4609ada19dceda7107350426971d52279a3ca
Trinity-L-Z <trinityzachariasen@gmail.com>
2025-05-04 13:36:09 -0500

    Update script.js

--- finalproject/script.js
+++ finalproject/script.js
@@ -1,7 +1,24 @@
+var song
+
+
+function preload() {
+    song = loadSound('machine.mp3');
+}
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
     background (0)
+}
+
+function togglePlaying() {
+    if (!song.isPlaying()) {
+        song.play();
+        button.html("pause");
+    } else {
+        song.pause();
+        button.html("play");
+    }
 }
